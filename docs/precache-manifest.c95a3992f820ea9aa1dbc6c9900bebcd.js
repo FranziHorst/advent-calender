52cca99847574322b4fecace3219f12363a674a5
Franzi Horst <franziska.horst-design@web.de>
2018-11-08 17:06:36 +0100

    Add first Version of calender

---
+++ docs/precache-manifest.c95a3992f820ea9aa1dbc6c9900bebcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aaf752cea96fca261453",
+    "url": "/static/js/main.aaf752ce.chunk.js"
+  },
+  {
+    "revision": "f4926859e068d12e2815",
+    "url": "/static/js/1.f4926859.chunk.js"
+  },
+  {
+    "revision": "aaf752cea96fca261453",
+    "url": "/static/css/main.892a1072.chunk.css"
+  },
+  {
+    "revision": "a8fc9c78f50eaaff13b1ddd2708b502a",
+    "url": "/index.html"
+  }
+];
